00e8fcd090ba3beb04c3315f8436dc538e5e671d
Scientistt <vitor.fabio10@gmail.com>
2018-12-08 12:00:41 -0400

    Atualização da página em tempo real sempre que o banco muda

---
+++ WebSite/data.php
@@ -0,0 +1,34 @@
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ufrr_dcc_ise2018";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM main_data WHERE id = (SELECT MAX(id) FROM main_data)";  //This is where I specify what data to query
+$result = mysqli_query($conn, $sql);
+
+$data = array();
+while($enr = mysqli_fetch_assoc($result)){
+    $a = array(
+        $enr['sensor_de_luz'], 
+        $enr['constante_sensor_de_luz'],
+        $enr['sensor_de_presenca'],
+        $enr['sensor_de_vibracao'],
+        $enr['constante_sensor_de_vibracao'],
+        $enr['data_e_hora']
+    );
+    array_push($data, $a);
+}
+
+echo json_encode($data);
+
+?>
